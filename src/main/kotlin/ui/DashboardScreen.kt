7e442c288bb4190238525b2c14abeef4ceb992ef
Saiful Islam <saifulsaif321@gmail.com>
2024-01-05 21:52:29 +0600

    Api call & image loaded

--- src/main/kotlin/ui/DashboardScreen.kt
+++ src/main/kotlin/ui/DashboardScreen.kt
@@ -25,19 +25,6 @@ import kotlinx.coroutines.yield
 import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalFoundationApi::class)
-fun PagerState.offsetForPage(page: Int) = (currentPage - page) + currentPageOffsetFraction
-
-@OptIn(ExperimentalFoundationApi::class)
-fun PagerState.startOffsetForPage(page: Int): Float {
-    return offsetForPage(page).coerceAtLeast(0f)
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-fun PagerState.endOffsetForPage(page: Int): Float {
-    return offsetForPage(page).coerceAtMost(0f)
-}
-
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DashboardScreen(
     data: DashboardData = DashboardData()
